04f08e636d334e0aebb0f108f996151b815f4ecc
abhishek_mourya <kartikmourya1947@gmail.com>
2025-10-02 23:16:03 +0530

    improving the token decode

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -2,18 +2,19 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = async (req, res, next) => {
 
-      const authHeader = req.headers['authorization'];
+  try {
+
+    const authHeader = req.headers['authorization'];
 
     // here we extract the token from the bearer token
     // we write the authHeader before && to check either authHeader is true or false
     // split() used to spread it and 1 means the second element of array
     const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
 
-  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded; // Attach full decoded token payload
     next();
